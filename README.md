016fda9863d49d862cc681486f1ecbdd6ab57f6b
RajeshThakur1 <rajesh@isl.net.in>
2022-04-30 09:17:03 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # text_classification_LSTM_degital_ocean
 In this we will do Text classification using LSTM and deploy in degital ocean
+
+### Create the virtual env
+```commandline
+conda create -p venv python==3.7 -y
+```
+
+### To install packages from setup.py
+```commandline
+pythoc setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+REMOVE_PACKAGE = ".e"
+REQUIREMENT_FILE_NAME= "requirements.txt"
+def get_requirement_list(requirement_file_name= REQUIREMENT_FILE_NAME) -> list:
+    try:
+        with open(requirement_file_name) as requirement_file:
+            requirement_list = [requirement.replace("\n","") for requirement in requirement_file.readlines()]
+            requirement_list.remove(REMOVE_PACKAGE)
+        return requirement_list
+    except Exception as e:
+        pass
+
+setup(
+    name= "lstm-text-classification",
+    version="0.0.1",
+    description= "LSTM based text classification project deploy in Degital ocean",
+    author="Rajesh Thakur",
+    packages=find_packages(),
+    install_requires=get_requirement_list()
+)
